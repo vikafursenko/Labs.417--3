945013065f258dc5eac0a6b83006885211f8105f
vikafursenko <73234317+vikafursenko@users.noreply.github.com>
2020-12-16 23:27:25 +0300

    Update Этап2.cpp

--- Этап2.cpp
+++ Этап2.cpp
@@ -1,26 +1,46 @@
 #include "pch.h"
 #include <iostream>
 
-int main() 
+double a[2];
+double b[2];
+double proizvedenie[1];
+double one()
 {
-	setlocale(LC_ALL, "Russian");
-	float a[2], b[2], scalar = 0.0;
-	int i;
-
-	std::cout << "Вектор a =  " << std::endl;
-	for (i = 0; i < 2; i++)
-		std::cin >> a[i];
-	
-	std::cout << "Вектор b =  " << std::endl;
-	for (i = 0; i < 2; i++)
-		std::cin >> b[i];
-	
-
-	for (i = 0; i < 2; i++)
-		scalar+= (a[i] * b[i]);
-	
-
-	std::cout << "Скалярное произведение = " << scalar << std::endl;
+	std::cout << "Введите 2 координаты вектора a:= " << std::endl;
+	std::cout << "x1 := ";
+	std::cin >> a[0];
+	std::cout << "y1 := ";
+	std::cin >> a[1];
 
 	return 0;
+}
+void two()
+{
+
+	std::cout << "Введите 2 координаты вектора b: = " << std::endl;
+	std::cout << "x2 := ";
+	std::cin >> b[0];
+	std::cout << "y2 := ";
+	std::cin >> b[1];
+
+}
+int resultat()
+{
+
+	proizvedenie[0] = (a[0] * b[0]) + (a[1] * b[1]);
+	std::cout << "Скалярное произведение векторов a и b: = " << std::endl;
+	std::cout << proizvedenie[0] << std::endl;
+
+
+	return 0;
+
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	one();
+	two();
+	resultat();
+
 }
