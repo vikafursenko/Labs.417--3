db8b43fde610bb66fa14645e8033dd9ac9068f60
vikafursenko <73234317+vikafursenko@users.noreply.github.com>
2020-12-16 16:09:25 +0300

    Update Этап2.cpp

--- Этап2.cpp
+++ Этап2.cpp
@@ -1,65 +1,26 @@
-﻿#include "pch.h"
+#include "pch.h"
 #include <iostream>
-#include <string>
 
-class scalar
-{
-public:
-	double a[2];
-	double b[2];
-	double proizvedenie[1];
-	std::string name;
-	void one()
-	{
-		std::cout << "Введите 2 координаты вектора a:= " << std::endl;
-		std::cout << "x1 := ";
-		std::cin >> a[0];
-
-		
-
-		std::cout << "y1 := ";
-		std::cin >> a[1];
-
-
-	}
-	void two()
-	{
-		std::cout << "Введите 2 координаты вектора b: = " << std::endl;
-		std::cout << "x2 := ";
-		std::cin >> b[0];
-
-		std::cout << "y2 := ";
-		std::cin >> b[1];
-
-	}
-	double resultat()
-	{
-		proizvedenie[0] = (a[0] * b[0]) + (a[1] * b[1]);
-
-		return 0;
-
-	}
-	double vivod()
-	{
-		std::cout << "Скалярное произведение векторов a и b: = " << std::endl;
-		std::cout << proizvedenie[0] << std::endl;
-
-
-		return 0;
-
-	}
-
-
-};
-int main()
+int main() 
 {
 	setlocale(LC_ALL, "Russian");
+	float a[2], b[2], scalar = 0.0;
+	int i;
 
-	scalar vector;
-	vector.one();
-	vector.two();
-	vector.resultat();
-	vector.vivod();
+	std::cout << "Вектор a =  " << std::endl;
+	for (i = 0; i < 2; i++)
+		std::cin >> a[i];
+	
+	std::cout << "Вектор b =  " << std::endl;
+	for (i = 0; i < 2; i++)
+		std::cin >> b[i];
+	
+
+	for (i = 0; i < 2; i++)
+		scalar+= (a[i] * b[i]);
+	
+
+	std::cout << "Скалярное произведение = " << scalar << std::endl;
+
 	return 0;
-	system("pause");
 }
